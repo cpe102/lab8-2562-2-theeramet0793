cd403d84acb3973539aa40aeb6389d11f3ca558a
theeramet0793 <theerametmetha@gmail.com>
2020-01-24 15:00:18 +0700

    commit-1

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 //Write the function printO() here
+void printO(int x,int y){
+ 
+     if(x<1||y<1) cout<<"Invalid input";
+	 else
+	 {
+		 for(int i = 1; i<= x; i++){
+			 for (int j = 1; j <= y; j++)
+			 {
+				 cout<<"O";
+			 }
+			 cout<<"\n";
+		 }
+	 }
+	 
+}
 
 int main(){
 	
