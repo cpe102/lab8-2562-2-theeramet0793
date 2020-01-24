13d6ab8cfdeee8e9083962e1ba517f5ee1a74544
theeramet0793 <theerametmetha@gmail.com>
2020-01-24 16:12:09 +0700

    commit-1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,7 +2,29 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
+double pay(double x,double y){
+		if(x>y){
+			return y;
+		}
+		else
+		{
+			return x;
+		}
+	}
+
+
 int main(){	
+	double initial_loan,rate_per_year,can_pay_per_year;
+	int round=1;
+	
+	cout<<" Enter initial loan: ";
+	cin>>initial_loan;
+	cout<<" Enter interest rate per year (%): ";
+	cin>>rate_per_year;
+	cout<<"Enter amount you can pay per year: ";
+	cin>>can_pay_per_year;
+
+    double PrevBalance=initial_loan,newBalance,payment,total;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -14,17 +36,25 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	// my functuion
+
+	
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
 	
+	do{
+	cout << fixed << setprecision(2); 
+	cout << setw(13) << left << round; 
+	cout << setw(13) << left << PrevBalance;
+	cout << setw(13) << left << PrevBalance*(rate_per_year/100);
+	cout << setw(13) << left << PrevBalance+(PrevBalance*(rate_per_year/100));
+	cout << setw(13) << left << pay(can_pay_per_year,PrevBalance+(PrevBalance*(rate_per_year/100)));
+	newBalance=(PrevBalance+(PrevBalance*(rate_per_year/100)))-(pay(can_pay_per_year,PrevBalance+(PrevBalance*(rate_per_year/100))));
+	PrevBalance=newBalance;
+	cout << setw(13) << left << newBalance ;
+	cout << "\n";
+	round=round+1;	
+	}while(newBalance>0);
 	return 0;
 }
